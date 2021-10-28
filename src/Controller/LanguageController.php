ed9748cb7e34933158a26a88aae251e9b77b6e47
AmsaluGit <tadesseamsalu@gmail.com>
2021-10-28 12:01:46 +0300

    done

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -3,35 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
-use App\Entity\Event;
-use App\Form\EventType;
-use App\Repository\EventRepository;
+use Symfony\Component\Routing\Annotation\Route;
 
 // use Symfony\Component\HttpFoundation\RequestEvent;
 
 class LanguageController extends AbstractController
 {
-    
+
     /**
      * @Route("/language", name="language", methods={"GET","POST"})
      */
 
-    public function index(Request $request) 
+    public function index(Request $request)
     {
-      
-        $request->getSession()->set('_locale',$request->query->get("lang"));
-       // $request->getSession()->set('_locale','fr');
-       
-      //  $route  = $request->get('_route');
 
-
-        return $this->redirectToRoute('home');
-        // return $this->redirectToRoute("home");
+        $request->getSession()->set('_locale', $request->query->get("lang"));
+        $next = $request->query->get("curr");
+        //$route  = $request->get('_route');
+        return $this->redirectToRoute($next);
     }
 
-     
 }
